b725db70f415942b361f4ab85b10e6797a7e2b2f
KyloJorgensen <kylo.j.jorgensen@gmail.com>
2016-05-30 20:24:05 -0700

    made it a gaome with prompts for user Input

--- js/app.js
+++ js/app.js
@@ -4,19 +4,32 @@ $(document).ready(function() {
 	for (var i = 1; i <= 100; i++) {
 		fizz = '';
 		buzz = '';
+		var userInput = prompt("#, Fizz, Buzz, or FizzBuzz", i);
+		var anwser = ''
 		if (i % 3 === 0) {
 			if (i % 5 === 0) {
-				$('ul').append('<li><p>FizzBuzz</p></li>')
+				anwser = 'FizzBuzz';
 			}
 			else {
-				$('ul').append('<li><p>Fizz</p></li>')
+				anwser = 'Fizz';
 			}
 		}
 		else if (i % 5 === 0) {
-			$('ul').append('<li><p>Buzz</p></li>')
+			anwser = 'Buzz';
 		}
 		else {
-			$('ul').append('<li><p>' + i + '</p></li>')
+			anwser = i ;
+		}
+
+		if (anwser == userInput) {
+			$('ul').append('<li><p>' + anwser + '--> Great Job</p></li>');
+			if (i == 100) {
+				$('ul').append('<li><p>YOU WIN!!!</p></li>');
+			}
+		}
+		else {
+			$('ul').append('<li><p>--> YOU LOSE!! correct anwser --> ' + anwser + '</p></li>');
+			i = 100;
 		}
 	}
 });
