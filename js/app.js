260dfc33af460e094d0fd50138b13115eceef9a8
KyloJorgensen <kylo.j.jorgensen@gmail.com>
2016-05-31 22:10:46 -0700

    add descriptions to app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 	var amount
 	
-	function fizzBuzz() {
+	function fizzBuzz() { // loops though the until inputed number.
 		for (var i = 1; i <= amount; i++) {
 
-			function appends(anwser) {
+			function appends(anwser) { // adds new words or numbers to the page.
 				$('ul').append('<li>' + anwser + '</li>');
 			}
-
+			// decides what to print. 
 			i % 3 !== 0 && i % 5 !== 0 ? appends(i) : false;
 			i % 15 === 0 ? appends('FizzBuzz'): false;
 			i % 3 === 0 ? appends('Fizz') : false;
@@ -15,14 +15,15 @@ $(document).ready(function() {
 		}	
 	}
 
-	while (true) {
+	while (true) { // loops prompt until an correct input is entered.
 		amount = prompt('Input a number, please.')
-		if (amount === 'stop') {
+		if (amount === 'stop') { //stops while loop if needed.
 			break;
 		}
 		amount +parseInt(amount)
 		if (amount % 1 === 0) {
 			fizzBuzz()
+			break;
 		}
 	}
 
