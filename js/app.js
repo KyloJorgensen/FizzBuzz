2d9022555bebf99878787a153452b3592cbd93d7
KyloJorgensen <kylo.j.jorgensen@gmail.com>
2016-05-31 21:59:29 -0700

    change program to request user imput for the amount of fizzbuzzing to do instead of the guessing game thing.

--- js/app.js
+++ js/app.js
@@ -1,35 +1,37 @@
 $(document).ready(function() {
-	var fizz
-	var buzz
-	for (var i = 1; i <= 100; i++) {
-		fizz = '';
-		buzz = '';
-		var userInput = prompt("#, Fizz, Buzz, or FizzBuzz", i);
-		var anwser = ''
-		if (i % 3 === 0) {
-			if (i % 5 === 0) {
-				anwser = 'FizzBuzz';
+	var amount
+	
+	function fizzBuzz() {
+		for (var i = 1; i <= amount; i++) {
+			// var userInput = prompt("#, Fizz, Buzz, or FizzBuzz", i);
+			var anwser = '';
+			if (i % 3 !== 0 && i % 5 !== 0) {
+				anwser = i;
 			}
-			else {
-				anwser = 'Fizz';
+			else if (i % 15 === 0) {
+					anwser = 'FizzBuzz';
 			}
-		}
-		else if (i % 5 === 0) {
-			anwser = 'Buzz';
-		}
-		else {
-			anwser = i ;
-		}
+			else if (i % 3 === 0) {
+					anwser = 'Fizz';
+			}
+			else if (i % 5 === 0) {
+				anwser = 'Buzz';
+			}
+			$('ul').append('<li>' + anwser + '</li>');
+		}	
+	}
 
-		if (anwser == userInput) {
-			$('ul').append('<li><p>' + anwser + '--> Great Job</p></li>');
-			if (i == 100) {
-				$('ul').append('<li><p>YOU WIN!!!</p></li>');
-			}
+	while (true) {
+		amount = prompt('Input a number, please.')
+		if (amount === 'stop') {
+			break;
 		}
-		else {
-			$('ul').append('<li><p>--> YOU LOSE!! correct anwser --> ' + anwser + '</p></li>');
-			i = 100;
+		amount +parseInt(amount)
+		if (amount % 1 === 0) {
+			fizzBuzz()
 		}
 	}
+
+
+	
 });
